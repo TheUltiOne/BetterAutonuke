dfc638adb36b7a73d1469fc6fb36b0fe1920baaf
TheUltiOne <TheUltiOne@users.noreply.github.com>
2020-12-31 13:39:52 -0500

    Update EventHandlers.cs

--- BetterAutonuke/EventHandlers.cs
+++ BetterAutonuke/EventHandlers.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 using Exiled.API.Features;
 using Exiled.Events.EventArgs;
@@ -10,33 +11,25 @@ namespace BetterAutonuke
 {
     class EventHandlers
     {
-        public string FilterString(int i)
-        {
-            if (BetterAutonuke.Instance.Config.Announcement.Contains("{minutes}"))
-            {
-                var message = BetterAutonuke.Instance.Config.Announcement.Replace("{minutes}", ((int)(BetterAutonuke.Instance.Config.Time / BetterAutonuke.Instance.Config.Intervals * i / 60)).ToString());
-                return message;
-            }
-            else
-            {
-                return BetterAutonuke.Instance.Config.Announcement;
-            }
-        }
-
         public IEnumerator<float> Autonuke()
         {
             for (var i = 0; i < BetterAutonuke.Instance.Config.Intervals; i++)
             {
-                Cassie.Message(FilterString(i));
+                var message = BetterAutonuke.Instance.Config.Announcement.Replace("{minutes}", ((int)(BetterAutonuke.Instance.Config.Time / BetterAutonuke.Instance.Config.Intervals * i / 60)).ToString());
+                Cassie.Message(message);
 
-                Map.Broadcast(5, $"<color=red><b>The Alpha Warhead</b></color> will detonate in <color=red><b>{(int)BetterAutonuke.Instance.Config.Time / BetterAutonuke.Instance.Config.Intervals * i / 60}</b></color> minutes.");
+                Map.Broadcast(5, $"<color=red><b>The Alpha Warhead</b></color> will detonate in <color=red><b>{(int)(BetterAutonuke.Instance.Config.Time / BetterAutonuke.Instance.Config.Intervals * i / 60)}</b></color> minutes.");
                 yield return Timing.WaitForSeconds(BetterAutonuke.Instance.Config.Time / BetterAutonuke.Instance.Config.Intervals);
+
+                Log.Info((int)BetterAutonuke.Instance.Config.Time / BetterAutonuke.Instance.Config.Intervals);
             }
 
             Warhead.Start();
             Warhead.IsLocked = true;
 
             Map.Broadcast(5, "<color=red><b>The Alpha Warhead</b></color> was <color=red><b>automatically enabled. It can't be turned off.</b></color>");
+
+            Log.Info($"AUTO: {(int)BetterAutonuke.Instance.Config.Time / BetterAutonuke.Instance.Config.Intervals}");
         }
 
         public void OnRoundStarted()
